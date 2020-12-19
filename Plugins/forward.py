b5290267fb4618585840c6c924389bc7df887cc9
Monk Tea <71178046+MonkTeam@users.noreply.github.com>
2020-12-19 17:23:15 +0530

    Add files via upload

---
+++ Plugins/forward.py
@@ -0,0 +1,12 @@
+import pyrogram
+from pyrogram import filters
+from bot import channelforward
+from config import Config 
+
+@channelforward.on_message(filters.channel)
+async def forward(c, m):
+    # Forwarding the messages to the channel
+    for id in Config.CHANNEL:
+       from_channel, to_channel = id.split(":")
+       if m.chat.id == int(from_channel):
+          await m.forward(int(to_channel), as_copy=True)
---
+++ translation.py
@@ -0,0 +1,10 @@
+class Translation(object):
+
+      # Start text
+      START = "**Hi I am a channel Broadcast Bot**"             
+
+      #About text
+      ABOUT = """
+<b>📝 Language:</b> Python 3
+<b>🧰 Framework:</b> Pyrogram
+<b>👨‍💻 Developer:</b> MonkAno"""
